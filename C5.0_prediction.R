d764e5fc0c4b0b667f4c76937ba87e53d211b4d9
Haydart <makowiecki.radoslaw@gmail.com>
2018-05-08 03:21:41 +0200

    Plot the learned tree & add tree algorithm modifiers

--- C5.0_prediction.R
+++ C5.0_prediction.R
@@ -5,11 +5,26 @@ library(caret)
 loadDataset <- function(filename) {
   dataset <<- read.csv(filename)
   class_col_index <<- which(names(dataset) == "class")
+  dataset[, "class"] <<- as.factor(dataset[, "class"])
 }
 
-loadDataset("datasets/iris.csv")
+plotExampleTree <- function(control_param) {
+  example_dec_tree = C5.0.default(dataset[,-class_col_index], dataset[,class_col_index], control = control_param)
+  plot(example_dec_tree)
+}
+
+loadDataset("datasets/glass.csv")
 dataset
 plot(dataset)
+
+tree_control <- C5.0Control(CF = 0.0)
+#tree_control <- C5.0Control(noGlobalPruning = TRUE)
+#tree_control <- C5.0Control(winnow = TRUE)
+#tree_control <- C5.0Control(minCases = 10)
+#tree_control <- C5.0Control()
+
+plotExampleTree(tree_control)
+
 
 folds <- createFolds(iris$Species, k = 10, list = FALSE)
 folds
